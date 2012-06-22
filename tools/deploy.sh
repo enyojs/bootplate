e87ef7a575fadd8bd89ca8b6e1930601a9046a8d
unwiredben <combee@techwood.org>
2012-06-21 21:58:23 -0500

    Modify bootplate scripts for Linux/Mac to change to right directory before running

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd `dirname $0`
+
 # the deploy target folder
 FOLDER=deploy
 
--- tools/minify.sh
+++ tools/minify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd `dirname $0`
+
 # build enyo
 pushd ../enyo/minify > /dev/null
 ./minify.sh
