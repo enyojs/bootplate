084aa6a13bcdfbbe94d0e500ffd8ae3d5c69b6a9
Kevin Schaaf <kevin.schaaf@lge.com>
2014-01-29 13:42:13 -0800

    Refactor bookplate skeleton per best-practice structure. DCO-1.1-Signed-Off-By: Kevin Schaaf <kevin.schaaf@lge.com>

--- source-old/package.js
+++ source-old/package.js
@@ -1,15 +1,7 @@
 enyo.depends(
-	// Layout library
 	"$lib/layout",
-	// Onyx UI library
 	"$lib/onyx",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
 	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
-	// CSS/LESS style files
-	"style",
-	// Model and data definitions
-	"data",
-	// View kind definitions
-	"views",
-	// Include our default entry point
-	"app.js"
+	"App.css",
+	"App.js"
 );
