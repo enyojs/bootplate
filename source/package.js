2cfbdef0c19539c119b707f7bb20fa9eb874ee4d
Roy Sutton <roy.sutton@lge.com>
2014-05-23 12:09:54 -0400

    Fix for ENYO-4043 -- remove references to Bootplate and clean up theming instructions.  Enyo-DCO-1.1-Signed-Off-By: Roy Sutton (roy.sutton@lge.com)

--- source/package.js
+++ source/package.js
@@ -3,7 +3,6 @@ enyo.depends(
 	"$lib/layout",
 	// Onyx UI library
 	"$lib/onyx",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
-	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
 	// CSS/LESS style files
 	"style",
 	// Model and data definitions
