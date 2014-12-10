cd17d92ff91f40d9b7469951e7c24b0e5b52fcc4
Jason Robitaille <robitaille88@gmail.com>
2014-12-10 09:46:51 -0600

    Consistant quotation usage

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,39 +4,36 @@
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
+ * 'License'); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
 */
 module.exports = function(grunt) {
-
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        clean: ["build", "deploy"],
+        clean: ['build', 'deploy'],
         'http-server': {
             'dev': {
                 // the server root directory
                 root: process.cwd(),
                 port: 8282,
-                host: "127.0.0.1",
+                host: '127.0.0.1',
                 cache: 0,
                 showDir : true,
                 autoIndex: true,
                 // server default file extension
-                ext: "html",
+                ext: 'html',
                 // run in parallel with other tasks
                 runInBackground:false
-
             }
-
         }
     });
 
