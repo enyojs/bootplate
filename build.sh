dc2fc7446baaaeef71921c9335f7f96f9b24376e
codepoet80 <jon.wise@jonandnic.com>
2022-02-23 15:00:50 -0500

    try change dir

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ echo "Building for Web..."
 $mydir/enyo-app/tools/deploy.sh
 
 if [ "$1" = "android" ]; then
+    echo "Building for Android..."
+    cd $mydir/cordova-app
+    `cordova platform add android`
     echo "Copying to Cordova..."
-    cp $mydir/enyo-app/deploy $mydir/cordova-app/www -R
+    cp $mydir/enyo-app/deploy/* $mydir/cordova-app/www -R
+
 fi
